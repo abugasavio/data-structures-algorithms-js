5d798a8201f383f4dca7ecc2b44d15112ef49a4a
Savio Abuga <savioabuga@gmail.com>
2017-04-01 15:12:40 +0300

    Pseudoclassical classes

---
+++ classes/01class.js
@@ -0,0 +1,10 @@
+function Building(floors) {
+    // this = {};
+    this.what = 'building';
+    this.floors = floors;
+    // return this;
+}
+
+var myBuilding = new Building(4);
+
+console.log(myBuilding);
