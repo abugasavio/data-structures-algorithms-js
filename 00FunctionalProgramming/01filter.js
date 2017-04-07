53d5c3d1685868c2c893ccd0035dee2155dbedfa
Savio Abuga <savioabuga@gmail.com>
2017-04-07 09:15:53 +0300

    map function

--- 00FunctionalProgramming/01filter.js
+++ 00FunctionalProgramming/01filter.js
@@ -8,8 +8,14 @@ const books = [
   }, {
     name: 'Advanced OOP in Python',
     langauge: 'Py',
+  }, {
+    name: 'Introduction to C++',
+    language: 'C++',
   },
 ];
+/*
+* Filter
+*/
 
 const isPython = function (book) {
   return book.langauge === 'Py';
@@ -18,4 +24,22 @@ const isPython = function (book) {
 const pythonBooks = books.filter(isPython);
 
 console.log(pythonBooks);
+
+/*
+* Map
+*/
+console.log('***** Using Map ********');
+const bookNames = [];
+
+for (let i = 0, len = books.length; i < len; i += 1) {
+  bookNames.push(books[i].name);
+}
+
+const bookNamesMap = books.map(book => `${book.name} is a ${book.langauge} book`);
+
+console.log(bookNames);
+console.log(bookNamesMap);
+
+
+console.log('**** End using Map ******');
 
