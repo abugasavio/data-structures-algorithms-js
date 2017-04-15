71eeae64e0344ea24fc5fb1c82dfc00da3bb9810
Savio Abuga <savioabuga@gmail.com>
2017-04-15 06:21:27 +0300

    add console logs to the stack code

--- 02stacksAndQueues/stackUsingObjectsAndCount.js
+++ 02stacksAndQueues/stackUsingObjectsAndCount.js
@@ -5,9 +5,12 @@ function Stack(capacity) {
 }
 
 Stack.prototype.push = function (val) {
-  this.count += 1;
-  this.storage[this.count] = val;
-  return this.count;
+  if (this.count < this.capacity) {
+    this.count += 1;
+    this.storage[this.count] = val;
+    return this.count;
+  }
+  return 'Max capacity already reached. Remove element before adding a new one.';
 };
 
 Stack.prototype.pop = function () {
@@ -21,7 +24,7 @@ Stack.prototype.peek = function () {
   return this.storage[this.count];
 };
 
-Stack.prototype.count = function () {
+Stack.prototype.size = function () {
   return this.count;
 };
 
@@ -30,9 +33,8 @@ console.log(myStack.push(1), 'should be 1');
 console.log(myStack.push(2), 'should be 2');
 console.log(myStack.push(4), 'should be 3');
 console.log(myStack.push(7), 'should be 4');
-
-
-
+console.log(myStack.push(5), 'should be Max capacity already reached. Remove element before adding a new one.');
+console.log(myStack.size(), 'should be 4');
 
 
 module.exports = Stack;
